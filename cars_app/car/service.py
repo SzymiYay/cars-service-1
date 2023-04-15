bb5c40a5770d564e1c58aee59179056260ed5e7c
SzymiYay <szymoon09@gmail.com>
2023-04-15 16:43:45 +0200

    Add docker-compose

--- cars_app/car/service.py
+++ cars_app/car/service.py
@@ -7,9 +7,14 @@ from operator import attrgetter
 from collections import defaultdict
 from copy import deepcopy
 
+
 @dataclass
 class CarsService:
     cars: list[Car]
+
+    def __str__(self):
+        to_string = [str(car) for car in self.cars]
+        return "\n".join(to_string)
 
     def sort_by(self, sort_criteria: SortCriteria, *, reverse=False) -> list[Car]:
         match sort_criteria:
@@ -25,6 +30,8 @@ class CarsService:
                 raise ValueError('Invalid sort criteria')
 
     def get_cars_with_mileage_greater_than(self, value: int) -> list[Car]:
+        if value < 0:
+            raise ValueError('Value must be greater than 0')
         return [car for car in self.cars if car.has_mileage_greater_than(value)]
 
     def get_color_and_no_of_cars(self) -> dict[Color, int]:
@@ -39,23 +46,14 @@ class CarsService:
         return {model: max(cars, key=lambda car: car.price) for (model, cars) in grouped_by_model.items()}
 
     def get_statistics(self) -> str:
-        return f"""
-                PRICE:
-                average: {sum([car.price for car in self.cars]) / len(self.cars)}
-                max: {max(self.cars, key=lambda car: car.price).price}
-                min: {min(self.cars, key=lambda car: car.price).price}
+        return self._get_statistics_price() + self._get_statistics_mileage()
 
-                MILEAGE:
-                average: {sum([car.mileage for car in self.cars]) / len(self.cars)}
-                max: {max(self.cars, key=lambda car: car.mileage).mileage}
-                min: {min(self.cars, key=lambda car: car.mileage).mileage}
-                """
-
-    def get_most_expensive_cars(self) -> Car | list[Car]:
+    def get_most_expensive_cars(self) -> list[Car]:
         grouped_by_price = defaultdict(list)
 
         for car in self.cars:
             grouped_by_price[car.price].append(car)
+
 
         return max(grouped_by_price.items(), key=lambda pair: pair[0])[1]
 
@@ -82,9 +80,29 @@ class CarsService:
 
         return [car for car in self.cars if car.has_price_between(price_from, price_to)]
 
-    def __str__(self):
-        to_string = [str(car) for car in self.cars]
-        return "\n".join(to_string)
-
     def _count_cars_with_given_color(self, color: Color) -> int:
         return len([car for car in self.cars if car.color == color])
+
+    def _get_statistics_price(self) -> str:
+        number_of_cars = len(self.cars)
+        average = sum([car.price for car in self.cars]) / number_of_cars if number_of_cars > 0 else 0
+        max_value = max(self.cars, key=lambda car: car.price).price if number_of_cars > 0 else 0
+        min_value = min(self.cars, key=lambda car: car.price).price if number_of_cars > 0 else 0
+        return f"""
+                PRICE:
+                average: {average}
+                max: {max_value}
+                min: {min_value}
+                """
+
+    def _get_statistics_mileage(self) -> str:
+        number_of_cars = len(self.cars)
+        average = sum([car.mileage for car in self.cars]) / number_of_cars if number_of_cars > 0 else 0
+        max_value = max(self.cars, key=lambda car: car.mileage).mileage if number_of_cars > 0 else 0
+        min_value = min(self.cars, key=lambda car: car.mileage).mileage if number_of_cars > 0 else 0
+        return f"""
+                MILEAGE:
+                average: {average}
+                max: {max_value}
+                min: {min_value}
+                """
